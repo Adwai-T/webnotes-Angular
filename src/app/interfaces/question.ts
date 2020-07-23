5838ec37137c45cf1e5d3749a94955a4b9bd324b
Adwai-T <waitforadi@gmail.com>
2020-07-23 22:43:30 +0530

    Added some Util Notes

---
+++ src/app/interfaces/question.ts
@@ -0,0 +1,6 @@
+export interface Question{
+    title:string;
+    description:string;
+    answer:number;
+    topic?:string;
+}
--- src/app/services/questions/questions.service.ts
+++ src/app/services/questions/questions.service.ts
@@ -1,9 +1,55 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { HttpClient, HttpParams, HttpHeaders } from '@angular/common/http';
+
+import { Question } from 'src/app/interfaces/question';
 
 @Injectable({
   providedIn: 'root'
 })
 export class QuestionsService {
 
-  constructor() { }
+  private getQuestionsUrl:string = 'https://adwait-webnotes.herokuapp.com/api/questions';
+  private postQuestionsUrl:string = 'https://adwait-webnotes.herokuapp.com/api/questions/addquestions';
+  private deleteQuestionsUrl:string = 'https://adwait-webnotes.herokuapp.com/api/questions/deletequestions';
+  private putQuestionsUrl:string = 'https://adwait-webnotes.herokuapp.com/api/questions/deletequestions?id=123';
+
+  constructor(private http: HttpClient) { }
+
+  //--- get Questions
+  public getQuestions(pageNumber:number, numberOfQuestions:number): Observable<Question[]>{
+
+    let options = {
+      params : new HttpParams({
+        fromObject: {
+          "page" : pageNumber.toString(),
+          "size" : numberOfQuestions.toString()
+        }
+      })
+    }
+
+    return this.http.get<Question[]>(this.getQuestionsUrl, options);
+
+  }
+
+  //--- post Questions
+  public addQuestions(){
+
+    let options = {
+      headers: new HttpHeaders({
+        "Authorization" : "Bearer "+ localStorage.getItem('jwt')
+      })
+    }
+
+  }
+
+  //--- delete Questions
+  public deleteQuestions(){
+
+  }
+
+  //--- put Questions
+  public editQuestion(){
+
+  }
 }
